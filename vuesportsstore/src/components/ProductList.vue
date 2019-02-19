6a51a6ac55e2b1c3230f208686949ec6db9ea76f
eoneoff <eoneoff@gmail.com>
2019-02-19 13:19:06 +0200

    added cart

--- vuesportsstore/src/components/ProductList.vue
+++ vuesportsstore/src/components/ProductList.vue
@@ -7,7 +7,13 @@
                     {{p.price | currency}}
                 </span>
             </h4>
-            <div class="card-text bg-white p-1">{{ p.description }}</div>
+            <div class="card-text bg-white p-1">
+                {{ p.description }}
+                <button class="btn btn-success btn-sm float-right"
+                    @click="handleProductAdd(p)">
+                    Add To Cart
+                </button>
+            </div>
         </div>
         <page-controls />
     </div>
@@ -15,7 +21,7 @@
 
 <script>
 
-import { mapGetters } from "vuex";
+import { mapGetters, mapMutations } from "vuex";
 import PageControls from "./PageControls";
 
 export default {
@@ -27,6 +33,13 @@ export default {
         currency(value) {
             return new Intl.NumberFormat("en-US",
             { style: "currency", currency: "USD" }).format(value);
+        }
+    },
+    methods: {
+        ...mapMutations({ addProduct: "cart/addProduct"}),
+        handleProductAdd(product){
+            this.addProduct(product);
+            this.$router.push("/cart");
         }
     }
 }
